5731e75755983f3a543e3ff28976b0e4be8f15e1
Gabriele Cerfoglio <gabriele@Gabrieles-MacBook-Pro.local>
2017-06-19 14:12:35 +0200

    Updated for keystone v3, other fixes

--- src/server/server.go
+++ src/server/server.go
@@ -19,6 +19,7 @@ type config struct {
 	USERNAME string
 	PASSWORD string
 	TENANT string
+	PROJECT string
 }
 
 // Config var
@@ -30,6 +31,11 @@ type message struct {
     Enabler_ID string
     Enabler_Version string
     Timestamp string
+}
+
+// Checking the status of the server
+func status(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, I'm here"))
 }
 
 // Main handler function
@@ -49,8 +55,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
     client := &http.Client{}
     
     // Creating keystone request for token
-    data := []byte(`{"auth":{"passwordCredentials":{"username": "`+conf.USERNAME+`", "password":"`+conf.PASSWORD+`"}, "tenantName":"`+conf.TENANT+`"}}`)
-    req, err := http.NewRequest("POST", ""+conf.KEYSTONE_HOST+":"+conf.KEYSTONE_PORT+"/v2.0/tokens", bytes.NewBuffer(data))
+    //data := []byte(`{"auth":{"passwordCredentials":{"username": "`+conf.USERNAME+`", "password":"`+conf.PASSWORD+`"}, "tenantName":"`+conf.TENANT+`"}}`)
+    data := []byte(`{"auth":{"identity":{"methods":["password"],"password":{"user":{"name":"`+conf.USERNAME+`","domain":{"id":"default"},"password":"`+conf.PASSWORD+`"}}},"scope":{"project":{"name":"`+conf.PROJECT+`","domain":{"id":"default"}}}}}`)
+    fmt.Println(`{"auth":{"identity":{"methods":["password"],"password":{"user":{"name":"`+conf.USERNAME+`","domain":{"id":"default"},"password":"`+conf.PASSWORD+`"}}},"scope":{"project":{"name":"`+conf.PROJECT+`","domain":{"id":"default"}}}}}`)
+    req, err := http.NewRequest("POST", ""+conf.KEYSTONE_HOST+":"+conf.KEYSTONE_PORT+"/v3/auth/tokens", bytes.NewBuffer(data))
     req.Header.Set("Content-Type", "application/json")
     
     // Requesting token
@@ -60,13 +68,14 @@ func handler(w http.ResponseWriter, r *http.Request) {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
-    if resp.StatusCode != 200 {
+    if resp.StatusCode != 201 {
     	fmt.Println("Error with Keystone: ", resp.Status, resp.Header, resp.Body)
     	http.Error(w, err.Error(), http.StatusInternalServerError)
     	return
     }
     
     // Extracting token
+    /*
     body, err := ioutil.ReadAll(resp.Body)
     if err != nil {
         fmt.Println("Failed to read response from Keystone: ", err)
@@ -86,13 +95,16 @@ func handler(w http.ResponseWriter, r *http.Request) {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
+    */
+    token := resp.Header.Get("X-Subject-Token")
     resp.Body.Close()
     
     // Creating request for Monasca
     data = []byte(`{"name": "GE_Heartbeat", "dimensions": {"id": "`+m.ID+`", "enabler_id": "`+m.Enabler_ID+`", "enabler_version": "`+m.Enabler_Version+`"}, "timestamp": `+m.Timestamp+`, "value": 1}`)
     req, err = http.NewRequest("POST", ""+conf.MONASCA_HOST+":"+conf.MONASCA_PORT+"/v2.0/metrics", bytes.NewBuffer(data))
     req.Header.Set("Content-Type", "application/json")
-    req.Header.Set("X-Auth-Token", token.(string))
+    //req.Header.Set("X-Auth-Token", token.(string))
+    req.Header.Set("X-Auth-Token", token)
     
     // Sending metric to Monasca
     resp, err = client.Do(req)
@@ -117,22 +129,25 @@ func setConfEnvVariables() {
 		conf.KEYSTONE_HOST = os.Getenv("KEYSTONE_HOST")
 	}
 	if os.Getenv("KEYSTONE_PORT") != "" {
-		conf.KEYSTONE_HOST = os.Getenv("KEYSTONE_PORT")
+		conf.KEYSTONE_PORT = os.Getenv("KEYSTONE_PORT")
 	}
 	if os.Getenv("MONASCA_HOST") != "" {
-		conf.KEYSTONE_HOST = os.Getenv("MONASCA_HOST")
+		conf.MONASCA_HOST = os.Getenv("MONASCA_HOST")
 	}
 	if os.Getenv("MONASCA_PORT") != "" {
-		conf.KEYSTONE_HOST = os.Getenv("MONASCA_PORT")
+		conf.MONASCA_PORT = os.Getenv("MONASCA_PORT")
 	}
 	if os.Getenv("USERNAME") != "" {
-		conf.KEYSTONE_HOST = os.Getenv("USERNAME")
+		conf.USERNAME = os.Getenv("USERNAME")
 	}
 	if os.Getenv("PASSWORD") != "" {
-		conf.KEYSTONE_HOST = os.Getenv("PASSWORD")
+		conf.PASSWORD = os.Getenv("PASSWORD")
 	}
 	if os.Getenv("TENANT") != "" {
-		conf.KEYSTONE_HOST = os.Getenv("TENANT")
+		conf.TENANT = os.Getenv("TENANT")
+	}
+	if os.Getenv("PROJECT") != "" {
+		conf.PROJECT = os.Getenv("PROJECT")
 	}
 }
  
@@ -150,7 +165,9 @@ func main() {
 	    os.Exit(1)
     }
     setConfEnvVariables()
+    fmt.Println(conf)
 	
     http.HandleFunc("/beat", handler)
+    http.HandleFunc("/status", status)
     http.ListenAndServe(":8080", nil)
 }
